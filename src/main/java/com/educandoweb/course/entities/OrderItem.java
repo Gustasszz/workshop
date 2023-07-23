48cbada2c9461a457f2c9511909b24bceb6e7df2
Gustavo Gonçalves <gustavogfigueira001@gmail.com>
2023-07-23 04:12:32 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -0,0 +1,88 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import com.educandoweb.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private OrderItemPK id = new OrderItemPK();
+	
+	private Integer quanitity;
+	private Double price;
+	
+	public OrderItem() {	
+	
+	}
+
+	public OrderItem(Order order, Product product, Integer quanitity, Double price) {
+		id.setOrder(order);
+		id.setProduct(product);
+		this.quanitity = quanitity;
+		this.price = price;
+	}
+
+	@JsonIgnore
+	public Order getOrder() {
+		return id.getOrder();
+	}
+	
+	public void setOrder(Order order) {
+		id.setOrder(order);
+	}
+	
+	public Product getProduct() {
+		return id.getProduct();
+	}
+	
+	public void setProduct(Product product) {
+		id.setProduct	(product);
+	}
+	
+	
+	public Integer getQuanitity() {
+		return quanitity;
+	}
+
+	public void setQuanitity(Integer quanitity) {
+		this.quanitity = quanitity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
